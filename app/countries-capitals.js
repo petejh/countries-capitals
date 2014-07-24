22f0fa5f7b47957a53f339ec7e457d2de4809ce6
petejh <petejh0@users.noreply.github.com>
2014-07-23 21:00:37 -0700

    Populate the table of countries
    Meets minimum requirements for the #/countries view:
      * Queries geoname.org for countries data
      * Caches the data
      * Tabulates country name, country code, capital, area in square
        kilometers, population, and continent code
      * Formats the area and population with commas after every third digit
      * Provides a link to the country detail view for each country
      * Provides a button that can be used to return to the home view

--- app/countries-capitals.js
+++ app/countries-capitals.js
@@ -1,10 +1,33 @@
 angular.module('CountriesCapitals', ['ngRoute'])
 
+  .constant('CNC_API_BASE', 'http://api.geonames.org')
+  .constant('CNC_COUNTRIES', '/countryInfoJSON')
+  .constant('CNC_USERNAME', 'petejh')
+
+  .factory('cnc_Countries', function($http, CNC_API_BASE, CNC_COUNTRIES, CNC_USERNAME) {
+    return function() {
+      var countries = [];
+      $http({
+        method: 'JSONP',
+        url: CNC_API_BASE + CNC_COUNTRIES,
+        params: {
+          username: CNC_USERNAME,
+          callback: 'JSON_CALLBACK'
+        },
+        cache: true
+      }).success(function(response) {
+          angular.copy(response.geonames, countries);
+        })
+      return countries;
+    };
+  })
+
   .config(function($routeProvider) {
     $routeProvider.when('/', {
       templateUrl: 'home.html'
     }).when('/countries', {
-      templateUrl: 'countries.html'
+      templateUrl: 'countries.html',
+      controller: 'CountriesCtrl'
     }).when('/countries/:country/capital', {
       templateUrl: 'capital.html',
       controller: 'CapitalCtrl',
@@ -25,6 +48,10 @@ angular.module('CountriesCapitals', ['ngRoute'])
     $rootScope.$on('$routeChangeError', function() {
       $location.path('/error');
     });
+  })
+
+  .controller('CountriesCtrl', function($scope, cnc_Countries) {
+    $scope.countries = cnc_Countries();
   })
 
   .controller('CapitalCtrl', function($scope, country) {
