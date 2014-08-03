85760a1d7653c412f3e60f6d84479fff69eceaba
petejh <petejh0@users.noreply.github.com>
2014-08-03 15:30:28 -0700

    Create gulp tasks to prepare the application for deployment
    Create a build/ directory. Copy html and images. Compress, concatenate,
    and version css and js files. Note: deploy to gh-pages doesn't quite
    work right until gulp-gh-pages incorporates a mechanism for supplying
    non-cached credentials to GitHub.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,11 @@
 var gulp = require('gulp');
 var webserver = require('gulp-webserver');
+var annotate = require('gulp-ng-annotate');
+var usemin = require('gulp-usemin');
+var minifyCss = require('gulp-minify-css');
+var rev = require('gulp-rev');
+var uglify = require('gulp-uglify');
+var ghPages = require('gulp-gh-pages');
 
 gulp.task('webserver', function() {
   gulp.src('app')
@@ -13,3 +19,30 @@ gulp.task('webserver', function() {
       fallback: 'app/index.html'
     }));
 });
+
+gulp.task('stage-html', function() {
+  gulp.src(['./app/**/*.html', '!./app/index.html'], { base: './app' })
+    .pipe(gulp.dest('build/'));
+});
+
+gulp.task('stage-images', function() {
+  gulp.src(['./app/**/*.gif'], { base: './app' })
+    .pipe(gulp.dest('build/'));
+});
+
+gulp.task('usemin', function() {
+  gulp.src('./app/index.html')
+    .pipe(usemin({
+      css: [minifyCss(), 'concat', rev()],
+      js: [annotate(), uglify(), rev()]
+    }))
+    .pipe(gulp.dest('build/'));
+});
+
+gulp.task('build', ['stage-html', 'stage-images', 'usemin']);
+
+gulp.task('deploy', function() {
+  gulp.src('./build/**/*')
+    .pipe(ghPages());
+});
+
